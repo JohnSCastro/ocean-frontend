1512699c7cf51e3ab1b202cd37445737c842f2ae
João Castro <107279696+JohnSCastro@users.noreply.github.com>
2024-06-07 13:10:30 +0000

    Implementação do componente Card.jsx e Importação para o App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
 import './App.css'
+import Card from './components/Card/Card'
 
 function App() {
   return (
     <>
-      <div className="card">
-        <h2>Rick Sanchez</h2>
-        <div className='tag'>
-          <h5>Status: Vivo</h5>
-          <h5>Espécie: Humano</h5>
-          <h5>Origem: Terra C-137</h5>
-        </div>
-        <img src='https://rickandmortyapi.com/api/character/avatar/1.jpeg'></img>
-      </div>
+    <Card />
     </>
   )
 }
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,13 @@
+export default function Card() {
+    return (
+        <div className="card">
+            <h2>Rick Sanchez</h2>
+            <div className='tag'>
+                <h5>Status: Vivo</h5>
+                <h5>Espécie: Humano</h5>
+                <h5>Origem: Terra C-137</h5>
+            </div>
+            <img src='https://rickandmortyapi.com/api/character/avatar/1.jpeg'></img>
+        </div>
+    )
+}
