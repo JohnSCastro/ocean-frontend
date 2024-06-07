b001c1731fda39866dd7f20994e9a280f01e48f4
João Castro <107279696+JohnSCastro@users.noreply.github.com>
2024-06-07 12:21:52 +0000

    Estilização

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,13 @@ import './App.css'
 function App() {
   return (
     <>
-      <div>
+      <div className="card">
         <h2>Rick Sanchez</h2>
+        <div className='tag'>
+          <h5>Status: Vivo</h5>
+          <h5>Espécie: Humano</h5>
+          <h5>Origem: Terra C-137</h5>
+        </div>
         <img src='https://rickandmortyapi.com/api/character/avatar/1.jpeg'></img>
       </div>
     </>
