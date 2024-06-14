43cf7eac94845d573ff1543297aafa341eb7cb50
João Castro <107279696+JohnSCastro@users.noreply.github.com>
2024-06-14 12:41:48 +0000

    Reestruturação para implementação de router

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,11 @@
-import { useEffect, useState } from 'react'
 import './App.css'
-import Card from './components/Card/Card'
+import Home from './pages/Home'
 
 function App() {
 
-
-  const [itens, setItens] = useState([])
-
-  async function carregarDados(){
-    const apiUrl = 'https://rickandmortyapi.com/api/character'
-    const response = await fetch(apiUrl)
-    const body = await response.json()
-    const results = body.results
-    setItens(results)
-    
-    console.log(results)
-  } 
-
-  useEffect(function(){
-    carregarDados()
-  }, [])
-  
-
-
   return (
     <>
-    <div className='cards'>
-      {itens.map(elemento => <Card item={elemento} key={elemento.name} />)}
-    </div>
-    
+      <Home />
     </>
   )
 }
